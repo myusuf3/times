b05b9769f5da84a340bbd72af135f62c83fa4ad3
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2015-04-26 23:21:35 -0400

    adding new tests

---
+++ times_test.go
@@ -0,0 +1,25 @@
+package times
+
+import (
+	"testing"
+	"time"
+
+	"github.com/myusuf3/times"
+)
+
+func TestNowFunc(t *testing.T) {
+	timeNow := time.Now().UTC().Truncate(time.Millisecond)
+	timesNow := times.Now()
+	timesNowTime := timesNow.Time.Truncate(time.Millisecond)
+	if timeNow != timesNowTime {
+		t.Fatalf("Excepted %s and %s to be equal", timesNowTime, timeNow)
+	}
+
+	if timesNow.Timezone != "UTC" {
+		t.Fatalf("Expected the timezone to be UTC but got %s instead", timesNow.Timezone)
+	}
+	location, _ := time.LoadLocation("UTC")
+	if timesNow.Location != location {
+		t.Fatalf("Expected the location to be %s but got %s instead", location, timesNow.Location)
+	}
+}
